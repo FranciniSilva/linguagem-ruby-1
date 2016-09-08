40aad0c3bfde4b430160f81d2414915494489486
FranciniSilva <franciniassis89@hotmail.com>
2016-09-08 20:58:39 -0300

    Condicionais, laços, programação imperativa

--- teste.rb
+++ teste.rb
@@ -3,3 +3,28 @@ puts 175 == 174
 puts "175" == 175
 puts "175" == "175"
 puts "175".to_i == 175
+
+puts "\n"
+
+puts 175 != 174
+puts 175 != 175
+
+puts "\n"
+
+puts 175 > 174
+puts 175 >= 175
+puts 175 < 176
+puts 175 <= 175
+
+for dedos in 1..3
+	puts "Contando"
+end
+
+
+#comentario
+
+=begin
+	comentario de varias linhas
+end
+
+
