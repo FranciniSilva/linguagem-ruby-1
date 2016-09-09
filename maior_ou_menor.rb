9bf5558788d138ad549c3c09c1d221ae2123655a
FranciniSilva <franciniassis89@hotmail.com>
2016-09-08 23:20:11 -0300

    O poder da extração de código

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,36 +1,53 @@
-puts "Bem vindo ao jogo da adivinhação"
-puts "Qual é o seu nome?"
-nome = gets
-puts "\n\n\n\n\n\n"
-puts "Começaremos o jogo para você, " + nome
+def da_boas_vindas
+	puts "Bem vindo ao jogo da adivinhação"
+	puts "Qual é o seu nome?"
+	nome = gets
+	puts "\n\n\n\n\n\n"
+	puts "Começaremos o jogo para você, " + nome	
+end
 
-puts "Escolhendo um número secreto entre 0 e 200..."
-numero_secreto = 175
-puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+def sorteia_numero_secreto
+	puts "Escolhendo um número secreto entre 0 e 200..."
+	sorteado = 175
+	puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+	sorteado #a ultima instrução de uma função é o que será retornado
+end
 
-limite_de_tentativas = 5;
-
-for tentativa in 1..5
+def pede_um_numero(tentativa, limite_de_tentativas)
 	puts "\n\n\n\n"
 	puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
 	puts "Entre com o numero"
 	chute = gets
 	puts "Será que acertou? Você chutou " + chute
+	chute
+end
 
-
+def verifica_se_acertou(numero_secreto, chute)
 	acertou = numero_secreto == chute.to_i
 
 	if acertou
 		puts "Acertou!"
-		break
+		return true
+	end
+
+	maior = numero_secreto > chute.to_i
+	if maior
+		puts "O número secreto é maior!"
 	else
-		maior = numero_secreto > chute.to_i
-		if maior
-			puts "O número secreto é maior!"
-		else
-			puts "O numero secreto é menor!"
-			
-		end
+		puts "O numero secreto é menor!"
+	end
+	false
+end
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+
+limite_de_tentativas = 5;
+
+for tentativa in 1..limite_de_tentativas
+	chute = pede_um_numero tentativa, limite_de_tentativas
+	if verifica_se_acertou numero_secreto, chute
+		break
 	end
 end
 
